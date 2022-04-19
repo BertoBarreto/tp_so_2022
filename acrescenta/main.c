110e31b13e35545601db9655a1e57ed9c68712eb
roberto barreto <you@example.com>
2022-04-19 13:29:31 +0100

    acrescenta fixed

--- acrescenta/main.c
+++ acrescenta/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
@@ -10,12 +11,12 @@ int main(int argc, char const *argv[])
   // Verificar se é recebido 3 argumentos, execução do programa, ficheiro de origem e ficheiro de destino
   if (argc > 3)
   {
-    perror("Demasiados argumentos");
+    write(STDERR_FILENO, "Muitos Argumentos!\nSintaxe: ./acrescenta origem destino\n", 57);
     exit(1);
   }
   if (argc < 3)
   {
-    perror("Poucos Argumentos");
+    write(STDERR_FILENO, "Poucos Argumentos!\nSintaxe: ./acrescenta origem destino\n", 57);
     exit(1);
   }
 
@@ -28,26 +29,32 @@ int main(int argc, char const *argv[])
 
   // array de char do tamanho de bufsiz para guardar o conteudo lido do ficheiro de origem
   char originContent[BUFSIZ];
+  int origemInexistenteSize = 25 + strlen(argv[1]);
+  int destinoInexistenteSize = 25 + strlen(argv[2]);
+  char ficheiroOrigemInexistente[origemInexistenteSize];
+  char ficheiroDestinoInexistente[destinoInexistenteSize];
 
   // abertura do ficheiro de origem verificando se este existe
   // caso não exista, é lançado um erro
   fdOrigin = open(origin, O_RDONLY);
   if (fdOrigin == -1)
   {
-    
-    perror("Ficheiro  inexistente");
+    snprintf(ficheiroOrigemInexistente, origemInexistenteSize, "Ficheiro %s inexistente\n", argv[1]);
+
+    write(STDERR_FILENO, ficheiroOrigemInexistente, origemInexistenteSize);
     exit(1);
   }
 
   // abertura do ficheiro de destino verificando se este existe
   // caso não exista, é lançado um erro
-  fdDestination = open(destination, O_WRONLY | O_APPEND);
+  fdDestination = open(destination, O_RDONLY);
   if (fdDestination == -1)
   {
-    perror("Ficheiro de destino inexistente");
+    snprintf(ficheiroDestinoInexistente, origemInexistenteSize, "Ficheiro %s inexistente\n", argv[2]);
+    write(STDERR_FILENO, ficheiroDestinoInexistente, destinoInexistenteSize);
     exit(1);
   }
-
+  fdDestination = open(destination, O_WRONLY | O_APPEND);
   do
   {
     // ler o ficheiro de origem e guardar o tamanho de bufsiz no array criado anteriormente
@@ -56,7 +63,7 @@ int main(int argc, char const *argv[])
 
     if (readBytes < 0)
     {
-      perror("Erro a ler o ficheiro de origem");
+      write(STDERR_FILENO, "Erro a ler o ficheiro de origem\n", 32);
       exit(1);
     }
 
@@ -65,7 +72,7 @@ int main(int argc, char const *argv[])
     writtenBytes = write(fdDestination, originContent, readBytes);
     if (writtenBytes < 0)
     {
-      perror("Erro a escrever no ficheiro de destino");
+      write(STDERR_FILENO, "Erro a escrever no ficheiro de destino\n", 40);
       exit(1);
     }
   } while (readBytes > 0);
@@ -74,13 +81,15 @@ int main(int argc, char const *argv[])
   closeOrigin = close(fdOrigin);
   if (closeOrigin < 0)
   {
-    perror("Erro a fechar o ficheiro de origem");
+
+    write(STDERR_FILENO, "Erro a fechar o ficheiro de origem\n", 36);
     exit(1);
   }
   closeDestination = close(fdDestination);
   if (closeOrigin < 0)
   {
-    perror("Erro a fechar o ficheiro de destino");
+
+    write(STDERR_FILENO, "Erro a fechar o ficheiro de destino", 36);
     exit(1);
   }
 
